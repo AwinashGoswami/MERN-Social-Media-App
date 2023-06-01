71271cde9b5de181c178c19b103986b4f315a157
awinash <awinashgoswami3@gmail.com>
2023-06-01 12:53:47 +0500

    navbar developed

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import React from "react";
+import Navbar from "scenes/navbar";
+
+const HomePage = () => {
+  return (
+    <Box sx={{backgroundColor:"red"}}>
+      <Navbar />
+    </Box>
+  );
+};
+
+export default HomePage;
